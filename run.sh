266baae6b1a12975fcf2e51730136fe2782fa7e1
Nasim Jamali <ubuntu@ip-172-31-9-138.ec2.internal>
2022-02-04 23:59:06 +0000

    added Mol Dev batches

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_20X_5Ch.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_MolDev_10X.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_YokogawaJapan_20X.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_MolDev_20X_4site.yml
 
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_MolDev_20X_9site.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_MolDev_20X_Adaptive.yml
