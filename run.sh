d0c740e63a9a8e5c0d0b81cdd1858a8fa6dc39ca
Nasim Jamali <ubuntu@ip-172-31-9-138.ec2.internal>
2022-02-04 22:09:12 +0000

    added Scope1_Yokogawa_US_20X_5Ch and Scope1_YokogawaJapan_20X batches

---
+++ run.sh
@@ -0,0 +1,4 @@
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_20X_5Ch.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_YokogawaJapan_20X.yml
+
