e675b7f04c8ee6a0d65ff082dfd5f0ec08d644ee
Nasim Jamali <ubuntu@ip-172-31-9-138.ec2.internal>
2022-02-05 02:40:06 +0000

    added PE Bin1 and Bin2 batches

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Nikon_20X.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin1_Confocal_3Plane.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Nikon_10X.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin1_Widefield_1Plane.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin1_Widefield_3Plane.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Confocal_1Plane.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Confocal_3Plane.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Widefield_1Plane.yml
+
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Widefield_3Plane.yml
