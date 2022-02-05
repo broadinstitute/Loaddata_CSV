f2c0622cc055590a835f84092c8328e95d3cbb5f
Nasim Jamali <ubuntu@ip-172-31-9-138.ec2.internal>
2022-02-05 03:34:16 +0000

    added Yokogawa US and Japan batches

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin1_Confocal_3Plane.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_Japan_40X.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin1_Widefield_1Plane.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_10X.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin1_Widefield_3Plane.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_20X_6Ch_BRO0117033.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Confocal_1Plane.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_20X_6Ch_BRO0117059.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Confocal_3Plane.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_20X_6Ch_BRO01177034.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Widefield_1Plane.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_Yokogawa_US_40X_BRO0117059.yml
 
-python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_Scope1_PE_Bin2_Widefield_3Plane.yml
+
